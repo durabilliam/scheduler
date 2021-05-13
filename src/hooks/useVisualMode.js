1b27e4a8bcdff7f631c71329a7989cbc7f5a5b91
William Perry <durabilliam@gamil.com>
2021-05-13 18:55:08 -0230

    created and visualMode tests hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+  const transition = (mode, replace = false) => {
+    if (replace) {
+      setHistory(prev => {
+        return [...prev.slice(0, prev.length - 1), mode]
+    })} 
+    else {
+      setHistory(prev => {
+        return [...prev, mode]
+    })}
+  }
+
+  const back = () => {
+    if (history.length < 2){
+      return; 
+    }
+    setHistory(prev => {
+      return  [...prev.slice(0, history.length -1)]
+    });
+    //setHistory(prev => [...prev.slice(0, history.length -1)]);
+    //setMode()
+  }
+  
+
+  return { mode: history[history.length-1], transition, back };
+
+ 
+
+
+
+
+}
